c63eff7ba348725307e308c80faeba5949bf98d6
Curt Arbtin <curt.a.arbtin.civ@army.mil>
2026-09-18 15:35:54 -0500

    add migration and update entities

--- src/main/java/mil/army/moda/college/institution/Institution.java
+++ src/main/java/mil/army/moda/college/institution/Institution.java
@@ -1,13 +1,17 @@
 package mil.army.moda.college.institution;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 public class Institution {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
     private String name;
 
